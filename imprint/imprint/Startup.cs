03e42fdad905897c6b3af0b8aeea3cf0f70ff8b0
deepanshu102 <deepanshujindal1021997@gmail.com>
2019-05-07 14:54:50 +0530

    initial branch

---
+++ imprint/imprint/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(imprint.Startup))]
+namespace imprint
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
