0761d9bc507ed2a7809f7df35ec04d0c4a653c34
deepanshu102 <deepanshujindal1021997@gmail.com>
2019-05-18 23:27:56 +0530

    Profile done

--- WebApplication1/WebApplication1/Default.aspx.cs
+++ WebApplication1/WebApplication1/Default.aspx.cs
@@ -1,19 +1,94 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using WebApplication1.Models;
 namespace WebApplication1
 {
     public partial class _Default : Page
     {
+        Connections parent;
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            parent = new Connections();
+            if (Session["user"] != null &&(!(IsPostBack)))
+            {
+                Profile();
+            }
         }
+        void Profile()
+        {
+            try
+            {
+                parent.Connection_establish();
+                parent.cmd = new System.Data.SqlClient.SqlCommand("Profile", Connections.con);
+                parent.cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                SqlParameter parameter = parent.cmd.Parameters.AddWithValue("@uid", SqlDbType.NVarChar);
+                parameter.Value = ((List<string>)Session["user"])[0].ToString();
+                parent.dr = parent.cmd.ExecuteReader();
+                if (parent.dr.Read())
+                {
+                    username.Text = parent.dr["uid"].ToString();
+                    name.Text = parent.dr["uname"].ToString();
+                    phone.Text = parent.dr["phone"].ToString();
+                    address.Text = parent.dr["address"].ToString();
+                    email.Text = parent.dr["email"].ToString();
 
-     
+                }
+                else
+                {
+
+                }
+
+            }
+            catch (Exception k)
+            {
+                Response.Write(k.StackTrace);
+            }
+            finally
+            {
+                parent.Connection_refuse();
+            }
+        }
+        protected void update_Click(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                try
+                {
+                    parent.Connection_establish();
+                    parent.cmd = new System.Data.SqlClient.SqlCommand("profile_updation", Connections.con);
+                    parent.cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    (parent.cmd.Parameters.AddWithValue("@userid", SqlDbType.NVarChar)).Value = username.Text.Trim();
+                    (parent.cmd.Parameters.AddWithValue("@uid", SqlDbType.NVarChar)).Value = ((List<string>)Session["user"])[0].ToString();
+                    (parent.cmd.Parameters.AddWithValue("@address", SqlDbType.NVarChar)).Value = address.Text.Trim();
+                    (parent.cmd.Parameters.AddWithValue("@phone", SqlDbType.NVarChar)).Value = phone.Text.Trim();
+                    (parent.cmd.Parameters.AddWithValue("@email", SqlDbType.NVarChar)).Value = email.Text.Trim();
+                    if ((int)parent.cmd.ExecuteNonQuery() > 1)
+                    {
+                        Response.Write("<script>alert('Sucessfully updated');</script>");
+                    }
+                    else
+                    {
+
+                        Response.Write("<script>alert('Some Issues');</script>");
+                    }
+                }
+                catch (Exception k)
+                {
+                    Response.Write(k.StackTrace);
+                }
+                finally
+                {
+                    parent.Connection_refuse();
+                    Profile();
+                }
+            }
+         
+        }
     }
 }
