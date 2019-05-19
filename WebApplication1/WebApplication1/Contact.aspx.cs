8d67d9c409042be140bae46be1219328ad2c4f77
deepanshu102 <deepanshujindal1021997@gmail.com>
2019-05-20 00:57:55 +0530

    changepassword run in Contact.aspx

--- WebApplication1/WebApplication1/Contact.aspx.cs
+++ WebApplication1/WebApplication1/Contact.aspx.cs
@@ -1,17 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
+using WebApplication1.Models;
 namespace WebApplication1
 {
     public partial class Contact : Page
     {
+        Connections parent;
         protected void Page_Load(object sender, EventArgs e)
         {
+            parent = new Connections();
+        }
 
+        protected void Update_Click(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                try
+                {
+
+                    parent.Connection_establish();
+                    parent.cmd = new System.Data.SqlClient.SqlCommand("changepassword", Connections.con);
+                    parent.cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    (parent.cmd.Parameters.AddWithValue("@uid", SqlDbType.NVarChar)).Value = ((List<string>)Session["user"])[0].ToString();
+                    (parent.cmd.Parameters.AddWithValue("@password", SqlDbType.NVarChar)).Value = oldpass.Text;
+                    (parent.cmd.Parameters.AddWithValue("@pass", SqlDbType.NVarChar)).Value = repass.Text;
+                    if (parent.cmd.ExecuteNonQuery() > 0)
+                    {
+                        Response.Write("<script>alert('Sucessfully update');</script>");
+                    }
+                    else
+                    {
+                        Response.Write("<script>alert('Invalid details');</script>");
+                    }
+
+                }
+                catch
+                { }
+                finally
+                { }
+            }
         }
     }
 }
