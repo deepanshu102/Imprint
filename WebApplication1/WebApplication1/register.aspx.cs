d726aa6d4a1160934c4b3476595a051c9394c6bf
deepanshu102 <deepanshujindal1021997@gmail.com>
2019-06-05 02:21:35 +0530

    Registeration done

--- WebApplication1/WebApplication1/register.aspx.cs
+++ WebApplication1/WebApplication1/register.aspx.cs
@@ -62,5 +62,70 @@ namespace WebApplication1
                 }
             }
         }
+
+        protected void register1_Click(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                try
+                {
+                    parent.Connection_establish();
+                    parent.cmd = new SqlCommand("register", Connections.con);
+                    parent.cmd.CommandType = CommandType.StoredProcedure;
+                    (parent.cmd.Parameters.AddWithValue("@userid", SqlDbType.NVarChar)).Value = r_username.Text;
+                    (parent.cmd.Parameters.AddWithValue("@address", SqlDbType.NVarChar)).Value = address.Text;
+                    (parent.cmd.Parameters.AddWithValue("@email", SqlDbType.NVarChar)).Value = email.Text;
+                    (parent.cmd.Parameters.AddWithValue("@phone", SqlDbType.NVarChar)).Value = phone.Text;
+                    (parent.cmd.Parameters.AddWithValue("@name", SqlDbType.NVarChar)).Value = name.Text;
+                    (parent.cmd.Parameters.AddWithValue("@pass", SqlDbType.NVarChar).Value) = r_password.Text;
+
+                    if ((int)parent.cmd.ExecuteNonQuery() > 0)
+                    {
+                        Response.Write("<script>alert('Sucessfully Register');</script>");
+                    }
+                    else
+                    {
+                        Response.Write("<script>alert('UnSucessfull Register');</script>");
+
+                    }
+
+                }
+                catch
+                {
+                }
+                finally
+                {
+                    parent.Connection_refuse();
+                }
+
+            }
+        }
+
+        protected void r_username_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                parent.Connection_establish();
+                parent.cmd = new SqlCommand("select uid from users where uid=@1", Connections.con);
+                parent.cmd.Parameters.AddWithValue("@1", r_username.Text);
+                parent.dr = parent.cmd.ExecuteReader();
+                if (parent.dr.HasRows)
+                {
+                    label1.Text = "This username already Exist";
+                }
+                else
+                {
+                    label1.Text = "You can take this username";
+                }
+
+            }
+            catch
+            {
+            }
+            finally
+            {
+                parent.Connection_refuse();
+            }
+        }
     }
 }
